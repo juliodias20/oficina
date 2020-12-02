79b4a7fe1c0aa84d6cfbe59c638f9096a7817005
juliodias20 <julio_dias@live.com>
2020-12-01 22:57:44 -0300

    Correção arquivos duplicados

--- src/br/com/maverick/service/model/LoginServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package br.com.maverick.service.model;
-
-import br.com.maverick.model.model.UsuarioModel;
-
-public interface LoginServiceInterface {
-	
-	UsuarioModel login(UsuarioModel usuarioModel);
-
-}
